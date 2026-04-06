e87e0964803bdeb4775631773791ac66a5b9cb99
yRestrict <max.tricolor123@gmail.com>
2026-04-05 21:46:24 -0300

    aaaa

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,7 +30,7 @@ class DatabaseSeeder extends Seeder
             // TagSeeder::class,
             // PostSeeder::class,
             // UserSocialLinkSeeder::class,
-            // SettingSeeder::class,
+            SettingSeeder::class,
         ]);
     }
 }
